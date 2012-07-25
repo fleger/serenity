6683252324a5068204fafd5647b34fbfd39cb53a
fleger <florian6.leger@laposte.net>
2012-07-25 11:13:23 +0200

    Help: ability to get help for a specific module

--- lib/actions/help.sh
+++ lib/actions/help.sh
@@ -18,7 +18,7 @@
 
 serenity.actions.help.help() {
 cat << EOM
-help [MODULE_TYPE]...
+help [MODULE_TYPE[.MODULE_NAME]]...
 
   Show help about serenity modules.
 
@@ -26,47 +26,87 @@ help [MODULE_TYPE]...
 
     MODULE_TYPE     show help for modules of MODULE_TYPE. Use the 'list' action to list the available
                     module types. [default: 'actions']
+
+    MODULE_NAME     show help for the module named MODULE_NAME of type MODULE_TYPE. Use the 'list MODULE_TYPE'
+                    action to list the available modules for the given MODULE_TYPE.
 EOM
 }
 
 serenity.actions.help.run() {
-  local -a apis=()
+  local -a args=()
   local indentation=""
 
   if [[ $# -eq 0 ]]; then
     echo "Usage: ${serenity_env_executable} ACTION"
     echo
-    apis+=("actions")
+    args+=("actions")
     indentation+="  "
   else
-    apis+=("$@")
+    args+=("$@")
   fi
-  
-  local api
+
+  local -A sections=()
+  local moduleType
+  local moduleName
   local item
-  local line
-  local helpFunc
 
-  for api in "${apis[@]}"; do
-    if ! serenity.tools.contains "$api" "${!serenity_APIs[@]}"; then
-      serenity.crash "$api is not a valid module type"
-    fi
-
-    echo "$indentation${api^*}:"
-    echo
-    indentation+="  "
-    
-    for item in $(serenity.tools.listFunctions "${serenity_APIs["$api"]}"); do
-      helpFunc="$(printf ${serenity_APIs_help["$api"]} "$item")"
-      if serenity.tools.isFunction "$helpFunc"; then
-        "$helpFunc" | while IFS= read line; do
-          echo "$indentation$line"
-        done
-        echo
-        echo
-      fi
-    done
-
-    indentation="${indentation%  }"
+  # Build sorted list
+  for item in "${args[@]}"; do
+    case "$item" in
+      *.*) # Specific module
+        moduleType="${item%.*}"
+        moduleName="${item##*.}"
+        if ! serenity.tools.contains "${!sections["$moduleType"]}" || [[ -n "${sections["$moduleType"]}" ]]; then
+          sections["$moduleType"]+=" $moduleName"
+        fi
+        ;;
+      *) # Whole module type
+        sections["$item"]=""
+        ;;
+    esac
   done
+
+  for moduleType in "${!sections[@]}"; do
+    serenity.actions.help.printModuleTypeHelp "$moduleType" ${sections["$moduleType"]}
+  done
+}
+
+serenity.actions.help.printModuleTypeHelp() {
+  local -a modules=()
+  local moduleType
+  local moduleName
+  moduleType="$1"
+  shift
+  if ! serenity.tools.contains "$moduleType" "${!serenity_APIs[@]}"; then
+    serenity.crash "$moduleType is not a valid module type"
+  fi
+
+  echo "$indentation${moduleType^*}:"
+  echo
+  indentation+="  "
+
+  if [[ "$#" -eq 0 ]]; then
+    readarray -t modules < <(serenity.tools.listFunctions "${serenity_APIs["$moduleType"]}")
+  else
+    modules=("$@")
+  fi
+
+  for moduleName in "${modules[@]}"; do
+    serenity.actions.help.printModuleHelp "$moduleType" "$moduleName"
+  done
+  
+  indentation="${indentation%  }"
+}
+
+serenity.actions.help.printModuleHelp() {
+  local helpFunc
+  local line
+  helpFunc="$(printf ${serenity_APIs_help["$1"]} "$2")"
+  if serenity.tools.isFunction "$helpFunc"; then
+    "$helpFunc" | while IFS= read line; do
+      echo "$indentation$line"
+    done
+    echo
+    echo
+  fi
 }
